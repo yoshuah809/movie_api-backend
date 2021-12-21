97fc375be993ca1320edc908b876f4daf1f70f08
Josue Garcia <yoshuah809@gmail.com>
2021-12-20 21:37:34 -0600

    Added DbContex and SQL initialization

---
+++ Contetx/MoviesDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace movies_api.Contetx
+{
+    public class MoviesDbContext
+    {
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using movies_api.Contetx;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,10 +30,8 @@ namespace movies_api
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "movies_api", Version = "v1" });
-            });
+            services.AddDbContext<MoviesDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("MoviesConnection")));
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,8 +40,7 @@ namespace movies_api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "movies_api v1"));
+                
             }
 
             app.UseHttpsRedirection();
