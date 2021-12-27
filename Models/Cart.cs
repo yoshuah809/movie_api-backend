65966e3a40953668dcf56b06caa785789d9aa0c2
Josue Garcia <yoshuah809@gmail.com>
2021-12-27 09:02:38 -0600

    added the favorite and cart models

---
+++ Models/Cart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace movies_api.Models
+{
+    public class Cart
+    {
+        public int UserID { get; set; }
+        public int MovieId { get; set; }
+        public User User { get; set; }
+        public Movie Movie { get; set; }
+    }
+}
+}
---
+++ Models/Favorites.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace movies_api.Models
+{
+    public class Favorites
+    {
+        public int UserID { get; set; }
+        public int MovieId { get; set; }
+        public User User { get; set; }
+        public Movie Movie {get; set; }
+    }
+}
